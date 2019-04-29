1b6e7b53a0d95f6cce46495354d4aa4360b3e4a0
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-04-28 22:27:54 -0700

    working version of carousel added in design

--- public/js/design.js
+++ public/js/design.js
@@ -5,47 +5,47 @@ function DesignPageFunctionality() {
   var imageArray = [
     {
       name: "vector1",
-      image: "url('img/vg1.svg')"
+      image: "img/vg1.svg"
     },
     {
       name: "vector2",
-      image: "url('img/vg2.svg')"
+      image: "img/vg2.svg"
     },
     {
       name: "vector3",
-      image: "url('img/vg3.svg')"
+      image: "img/vg3.svg"
     },
     {
       name: "vector4",
-      image: "url('img/vg4.svg')"
+      image: "img/vg4.svg"
     },
     {
       name: "vector5",
-      image: "url('img/vg5.svg')"
+      image: "img/vg5.svg"
     },
     {
       name: "vector6",
-      image: "url('img/vg6.svg')"
+      image: "img/vg6.svg"
     },
     {
       name: "vector7",
-      image: "url('img/vg7.svg')"
+      image: "img/vg7.svg"
     },
     {
       name: "vector8",
-      image: "url('img/vg8.svg')"
+      image: "img/vg8.svg"
     },
     {
       name: "vector9",
-      image: "url('img/vg9.svg')"
+      image: "img/vg9.svg"
     },
     {
       name: "vector10",
-      image: "url('img/vg10.svg')"
+      image: "img/vg10.svg"
     },
     {
       name: "vector11",
-      image: "url('img/vg11.svg')"
+      image: "img/vg11.svg"
     }
   ];
 
@@ -53,26 +53,24 @@ function DesignPageFunctionality() {
     var buttonLeft = document.getElementById('btn_left');
     var buttonRight = document.getElementById('btn_right');
     var designDisplayDiv = document.getElementById('design_vectors');
-    var i = 0;
+    
+    var image = "<img src='" + imageArray[0].image + "' class='image-dim' />";
+    designDisplayDiv.innerHTML = image;
+    var i = 1;
 
     buttonRight.addEventListener("click", function () {
-      var imageDisplay = imageArray[i];
-      console.log(imageDisplay);
-      i = i + 1;
-      
-      var image = "<img src='" + imageArray[i].image + "' />"
+      var imageDisplay = imageArray[Math.abs(i)];
+      i = (i + 1) % imageArray.length;
+      var image = "<img src='" + imageDisplay.image + "' class='image-dim' />";
       designDisplayDiv.innerHTML = image;
-      console.log(image);
-  
     });
-//
-//    buttonLeft.addEventListener("click", function () {
-//      var imageDisplay = imageArray[i];
-//      i = i - 1;
-//      designDisplayDiv.innerHTML = "<img src='imageArray[i].image' /";
-//      var image = "<img src='" + imageArray[i].image + "' />"
-//      console.log(image);
-//    });
+
+    buttonLeft.addEventListener("click", function () {
+      var imageDisplay = imageArray[Math.abs(i)];
+      i = (i - 1) % imageArray.length;
+      var image = "<img src='" + imageDisplay.image + "' class='image-dim' />";
+      designDisplayDiv.innerHTML = image;
+    });
   }
   carousel();
 }
