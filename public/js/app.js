d62c8a0461765c0bbac0da1ac0769474f556dced
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-05-13 16:44:43 -0700

    Added About me and Contact pages

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 function App() {
   Header();
-  if(window.location.href.match('index.html') != null) {
+  if (window.location.href.match("index.html") != null) {
     ScaleImage();
   }
-  
-  if(window.location.href.match('design.html') != null) {
+
+  if (window.location.href.match("design.html") != null) {
     DesignPageFunctionality();
   }
 }
